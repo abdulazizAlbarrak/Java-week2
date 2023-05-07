4996bcc058a356ac75b38f180397a244cfdbe3a2
Abdulaziz Albarrak <70835804+abdulazizAlbarrak@users.noreply.github.com>
2023-05-07 18:37:40 +0300

    Week2 HW1

--- Week2_HW1.java
+++ Week2_HW1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class Week2_HW1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         try {
