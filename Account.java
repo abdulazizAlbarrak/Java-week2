816ae665ed19c8a7c8d383e4f14a64c05ae0eaff
Abdulaziz Albarrak <70835804+abdulazizAlbarrak@users.noreply.github.com>
2023-05-08 11:44:21 +0100

    week2 - HW7

---
+++ Account.java
@@ -0,0 +1,54 @@
+public class Account {
+    private String id;
+    private String name;
+    private int balance = 0;
+
+
+    Account(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    Account(String id, String name, int balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int credit(int amount){
+        balance = balance + amount;
+        return balance;
+    }
+    public int debit(int amount){
+        if(balance - amount < 0) {
+            balance = balance - amount;
+            return balance;
+        }else return balance;
+    }
+
+    public int transferTo(Account another, int amount){
+        balance = balance - another.credit(amount);
+        return balance;
+    }
+
+    @Override
+    public String
+    toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
